87c063eefceb57e797236a7beeb363f46f689eea
Lupercal13 <hpayne13@gmail.com>
2023-01-31 11:47:15 +0900

    update the server url

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fet data from server == bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-ai-chat-bot.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
