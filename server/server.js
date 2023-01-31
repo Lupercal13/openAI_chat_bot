fead80c735676125847f5bf0772e3ab3b197659e
Lupercal13 <hpayne13@gmail.com>
2023-01-31 13:14:16 +0900

    change api key

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
@@ -17,13 +17,14 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.status(200).send({
-    message: "Hello from CodeX",
+    message: "Hello from CodeX!",
   });
 });
 
 app.post("/", async (req, res) => {
   try {
     const prompt = req.body.prompt;
+
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
